7780046d3841d3f88b7f3191a8cc680cb9fdf5d6
danianise <danianisehoey@gmail.com>
2022-04-16 21:29:21 -0400

    Added functionality to add and delete. Edit still needs to be debugged.

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -3,21 +3,47 @@ const router = express.Router()
 
 const Recipe = require('../models/recipe-model')
 
+//index route, shows all the recipes with images
 router.get('/', (req, res) => {
     Recipe.find({})
         .then(recipes => res.render('index', {recipes: recipes}))
         .catch(console.error)
 })
 
+//add recipe route, shows the form to add a recipe
 router.get('/add', (req, res) => {
     res.render('add')
 })
 
-router.post('recipes/add', (req, res) => {
+//POST route for adding a recipe when you submit the form
+router.post('/add', (req, res) => {
     console.log(req.body)
     Recipe.create(req.body)
-    .then(() => res.redirect('/'));
+    .then(() => res.redirect('/recipes'));
 })
 
+router.get('/:id/edit', (req, res) => {
+    Recipe.findOne({_id: req.params.id})
+        .then((recipes) => res.render('edit', {recipes: recipes}))
+})
+
+router.put('/:id/edit', (req, res) => {
+    const id = req.params.id
+    console.log(req.body)
+    Recipe.findOneAndUpdate({_id: id}, req.body)
+    .then(() => res.redirect('/recipes/id'))
+})
+
+//route to show each recipe individually
+router.get('/:id', (req, res) => {
+    Recipe.findOne({_id: req.params.id})
+    .then((recipes) => res.render('each', {recipes: recipes}))
+})
+
+//route to delete a recipe
+router.delete('/:id', (req, res) => {
+    Recipe.findOneAndRemove({ _id: req.params.id })
+    .then(() => res.redirect('/recipes') );
+});
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 require('dotenv').config()
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 const recipeControllers = require('./controllers/recipes')
 const Recipe = require('./models/recipe-model')
 const mongoose = require('./db/connection')
 const ejs = require('ejs')
-const methodOverride = require('method-override')
+
+
+app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 app.use('/recipes', recipeControllers)
-
-app.set('view engine', 'ejs')
 
 const port = process.env.PORT || 4000
 // app.set("port", process.env.PORT || 4000)
