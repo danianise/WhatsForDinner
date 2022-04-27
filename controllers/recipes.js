503441612476a5b56e3bc9ccc6fb6351c3f6894b
danianise <danianisehoey@gmail.com>
2022-04-26 23:15:26 -0400

    Added allergen filter feature, worked on styling

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,7 +1,10 @@
+const { response } = require('express')
 const express = require('express')
 const router = express.Router()
 
 const Recipe = require('../models/recipe-model')
+
+let allIngredients = []
 
 //INDEX route, shows all the recipes with images
 router.get('/', (req, res) => {
@@ -18,15 +21,24 @@ router.get('/add', (req, res) => {
 
 //POST route for ADDing a recipe when you submit the form
 router.post('/add', (req, res) => {
-    // console.log(req.body)
-    Recipe.create(req.body)
+    console.log(req.body.ingredients)
+    let ingredients = req.body.ingredients
+    let ingredientsArray = ingredients.split(", ")
+    console.log(ingredientsArray)
+
+    Recipe.create({
+        title: req.body.title,
+        ingredients: ingredientsArray,
+        imageURL: req.body.imageURL,
+        directionsURL: req.body.directionsURL,
+        allergensContained: req.body.allergensContained
+    })
     .then(() => res.redirect('/recipes'));
 })
 
 //route for INGREDIENT SEARCH FORM
 router.get('/ingredientSearchForm', (req, res) => {
-    Recipe.find({})
-        .then(recipes => res.render('ingredientSearchForm', {recipes: recipes}))
+    res.render('ingredientSearchForm')
 })
 
 //route for INGREDIENT SEARCH RESULTS page
@@ -43,8 +55,31 @@ router.post('/ingredientSearchResults', (req, res) => {
     Recipe.find({ingredients: { $all: formattedIngredients } })
         .then(searchResults => {
             // console.log(searchResults)
-            res.render('ingredientSearchResults', {searchResults: searchResults}) })
+            res.render('ingredientSearchResults', {searchResults: searchResults})
+        })
         .catch(console.error)
+})
+
+//route for ALLERGEN FILTER
+router.get('/allergenFilter', (req, res) => {
+    res.render('allergenFilter')
+})
+
+//route for ALLERGEN FILTER RESULTS page
+router.get('/allergenFilterResults', (req, res) => {
+    res.render('allergenFilterResults')
+})
+
+//POST route for submitting ALLERGEN FILTER form
+router.post('/allergenFilterResults', (req, res) => {
+  console.log(req.body.allergensContained)
+  let allergens = req.body.allergensContained
+//   Recipe.find({allergensContained: {$not: {$in: [allergens]} } })
+    Recipe.find({allergensContained: {$nin: [allergens]} })
+    .then(searchResults => {
+        res.render('allergenFilterResults', {searchResults: searchResults})
+    })
+    .catch(console.error)
 })
 
 //route for EDIT FORM
